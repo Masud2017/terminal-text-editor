f4c67f913e06ae9ef641cf821f3c933345392cd8
Masud karim <msmasud578@gmail.com>
2021-06-03 10:11:49 -0800

    cleaning stuff that I don't need

--- main.c
+++ main.c
@@ -79,11 +79,11 @@ void editorOpen(char* fileName) {
 	free(line);
 	fclose(fp);
 
-	E.row.size = linelen;
-	E.row.chars = malloc(linelen + 1);
-	memcpy(E.row.chars,line,linelen);
-	E.row.chars[linelen] = '\0';
-	E.numrows = 1;
+	// E.row.size = linelen;
+	// E.row.chars = malloc(linelen + 1);
+	// memcpy(E.row.chars,line,linelen);
+	// E.row.chars[linelen] = '\0';
+	// E.numrows = 1;
 }
 
 void editorMoveCursor(int key) {
